486e38c8a993f4c9dae85ab9d97e26468717f8c2
sanja0508 <pawarsanjana2006@gmail.com>
2025-08-07 15:40:45 +0530

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "What's your name? ";
+    cin >> name;
+
+    cout << "Hello, " << name << "! Welcome to the world of C++!" << endl;
+
+    return 0;
+}
